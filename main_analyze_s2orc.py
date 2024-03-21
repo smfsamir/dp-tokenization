e092448e2f3ff0ca5a91fb1d6544ed76772c495d
Farhan Samir <f.samir@mail.utoronto.ca>
2024-03-21 15:28:54 -0700

    update

--- main_analyze_s2orc.py
+++ main_analyze_s2orc.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 from huggingface_hub import snapshot_download, login
 from flowmason import conduct
 from datasets import load_dataset
+from transformers import TrainingArguments, Trainer, DataCollatorForSeq2Seq
 
 
 from inspect_tokenizer import compute_shortest_tokenizations
@@ -27,8 +28,39 @@ def step_download_olmo_model(**kwargs):
 
 def step_iterate_dataset(**kwargs):
     # load the dataset
-    dataset = load_dataset("leminda-ai/s2orc_small", cache_dir=SCRATCH_DIR)
+
     ipdb.set_trace()
+
+def step_finetune_llama(**kwargs):
+    # load the first 10 percent as eval dataset
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir=SCRATCH_DIR)
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir=SCRATCH_DIR)
+    eval_dataset = load_dataset("leminda-ai/s2orc_small", split='train[:10%]', cache_dir=SCRATCH_DIR)
+    train_dataset = load_dataset("leminda-ai/s2orc_small", split='train[10%:]', cache_dir=SCRATCH_DIR)
+    collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+    training_args = TrainingArguments(
+        output_dir=f"{SCRATCH_DIR}/llama_7b_hf_finetuned",
+        learning_rate=1e-3,
+        per_device_train_batch_size=32,
+        per_device_eval_batch_size=32,
+        num_train_epochs=2,
+        weight_decay=0.01,
+        torch_dtype='bfloat16',
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        load_best_model_at_end=True,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        tokenizer=tokenizer,
+        data_collator=collator
+    )
+    trainer.train()
+
+
 
 if __name__ == '__main__':
     cache_location = os.getenv("CACHE_DIR")
