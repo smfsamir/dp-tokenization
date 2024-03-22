3e390a95674814134fc9f5fb60d6b2ebd0a4d034
Farhan Samir <f.samir@mail.utoronto.ca>
2024-03-21 20:10:44 -0700

    update

--- main_analyze_s2orc.py
+++ main_analyze_s2orc.py
@@ -80,6 +80,7 @@ def step_finetune_llama(**kwargs):
         learning_rate=1e-4,
         per_device_train_batch_size=4,
         per_device_eval_batch_size=4,
+        gradient_checkpointing=True,
         optim="paged_adamw_32bit",
         gradient_accumulation_steps=2,
         num_train_epochs=2,
