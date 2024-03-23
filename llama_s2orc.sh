99914334724dd7c8c03f5a3706a30fe679b76185
Farhan Samir <f.samir@mail.utoronto.ca>
2024-03-22 17:46:48 -0700

    update

---
+++ llama_s2orc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=scrape_lgbt_wiki
+#SBATCH --mail-type=END,FAIL,INVALID_DEPEND
+#SBATCH --mail-user=smfsamir@uw.edu
+#SBATCH --account=argon
+#SBATCH --partition=gpu-l40
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=256G
+#SBATCH --gpus=1
+#SBATCH --time=168:00:00
+#SBATCH --export=all
+#SBATCH --output=/gscratch/argon/smfsamir/s2orc_llama_basic.out
+#SBATCH --error=/gscratch/argon/smfsamir/s2orc_llama_basic.error
+# Modules to use (optional).
+source /gscratch/argon/smfsamir/uw_samir_env/bin/activate
+# Your programs to run.
+cd dp-tokenization
+python main_analyze_s2orc.py
