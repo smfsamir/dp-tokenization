7bd066a53e476d591d99ad97f4c484be6b9c16cf
Farhan Samir <f.samir@mail.utoronto.ca>
2024-05-16 16:43:49 -0700

    assess collator

--- main_biomed_translation.py
+++ main_biomed_translation.py
@@ -197,7 +197,7 @@ def step_train_model(
         generation_max_length=128,
     )
     data_collator = DataCollatorForLanguageModeling(
-        default_tokenizer, max_length=1024, mlm=False
+        default_tokenizer, mlm=False
     )
     trainer = transformers.Seq2SeqTrainer(
         model=model,
