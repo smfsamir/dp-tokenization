01e2223f054ec9b220eed34530d47b8a8e60040d
Farhan Samir <f.samir@mail.utoronto.ca>
2024-05-20 12:08:49 -0700

    assess collator

--- main_biomed_translation.py
+++ main_biomed_translation.py
@@ -107,7 +107,7 @@ class ShortcutDataCollatorForSeq2Seq(DataCollatorForLanguageModeling):
 
         input_ids = []
         for datapoint in features:
-            input_ids.append(torch.Tensor(datapoint['input_ids'] + datapoint['labels']).int())
+            input_ids.append(torch.Tensor(datapoint['input_ids'] + datapoint['labels']))
             # input_ids.append(torch.Tensor(datapoint['input_ids'] + datapoint['labels']))
         input_ids = pad_sequence(input_ids, padding_value=self.tokenizer.pad_token_id, batch_first=True).to('cuda')
         # input_ids.requires_grad = False
@@ -117,10 +117,9 @@ class ShortcutDataCollatorForSeq2Seq(DataCollatorForLanguageModeling):
         # for feature in features_new['labels']:
         #     tensors.append(torch.Tensor(feature).int())
         # label_tensors = pad_sequence(tensors, padding_value=self.tokenizer.pad_token_id).to('cuda')
-        ipdb.set_trace()
         padded_features = {
-            'input_ids': input_ids,
-            'labels': input_ids.float()
+            'input_ids': input_ids.int(),
+            'labels': input_ids
         }
         return padded_features
 
